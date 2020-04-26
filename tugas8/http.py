89de6de5e0bbd6787c3111d169afd5ff449e4e6c
Bayu Laksana <bynd4spd@gmail.com>
2020-04-26 21:54:22 +0700

    refactor coding style

--- tugas8/http.py
+++ tugas8/http.py
@@ -16,7 +16,7 @@ class HttpServer:
 	
 	def response(self,kode=404,message='Not Found',messagebody='',headers={}):
 		tanggal = datetime.now().strftime('%c')
-		resp=[]
+		resp = []
 		resp.append("HTTP/1.0 {} {}\r\n" . format(kode,message))
 		resp.append("Date: {}\r\n" . format(tanggal))
 		resp.append("Connection: close\r\n")
@@ -28,7 +28,7 @@ class HttpServer:
 		
 		resp.append("\r\n")
 		resp.append("{}" . format(messagebody))
-		response_str=''
+		response_str = ''
 		
 		for i in resp:
 			response_str="{}{}" . format(response_str,i)
@@ -40,20 +40,19 @@ class HttpServer:
 		baris = requests[0]
 		
 		all_headers = [n for n in requests[1:] if n != '']
-		j = baris.split(" ")
+		requestArr = baris.split(" ")
 		try:
-			method = j[0].upper().strip()
+			method = requestArr[0].upper().strip()
 
 			if method == 'GET':
-				object_address = j[1].strip()
-				object_address = object_address.replace('/', '')
+				object_address = requestArr[1].strip()
 				print(object_address)
 				return self.http_get(object_address, all_headers)
 			
 			if method == 'POST':
 				form = requests[-1].split("=")
 				formData = form[1]
-				object_address = j[1].strip()
+				object_address = requestArr[1].strip()
 				return self.http_post(object_address, all_headers, formData)
 			else:
 				return self.response(400,'Bad Request','',{})
@@ -62,23 +61,20 @@ class HttpServer:
 			return self.response(400,'Bad Request','',{})
 	
 	def http_get(self,object_address,headers):
-		# files = glob('./*')
-		# thedir='.\\'
-		# print(thedir+object_address)
-		# if thedir+object_address not in files:
-		# 	return self.response(404,'Not Found','',{})
-		# fp = open(thedir+object_address,'r')
-		if object_address=='sending.html':
-			fp = open("sending.html", 'r')
-			isi = fp.read()
-		else:
-			isi = '<h1>SERVER HTTP</h1>'
+		files = glob('./*')
+		thedir = '.'
+		print(thedir+object_address)
+		
+		if thedir+object_address not in files:
+			return self.response(404,'Not Found','',{})
+		
+		fp = open(thedir + object_address, 'r')
+		isi = fp.read()
+		fext = os.path.splitext(thedir + object_address)[1]
+		content_type = self.types[fext]
 
-		# fext = os.path.splitext(thedir+object_address)[1]
-		# content_type = self.types[fext]
-
-		headers={}
-		headers['Content-type']= "text/html"
+		headers = {}
+		headers['Content-type'] = "text/html"
 
 		return self.response(200,'OK',isi,headers)
 
